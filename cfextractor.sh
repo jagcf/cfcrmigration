4d9aafc7adaeac3b3911c72369850c9fd52d4a84
Jag Shetty <59068693+jagcf@users.noreply.github.com>
2020-04-13 19:40:57 -0700

    Update cfextractor.sh

--- cfextractor.sh
+++ cfextractor.sh
@@ -20,168 +20,94 @@ functionApiCall(){
    fi
 } 
 
-function1 () {
-  echo "starting retrieveing all repo urls"
+function1 ()  {
 
-  pipsMetaUrl="https://g.codefresh.io/api/pipelines?includeBuilds=false&limit=200&offset=0"
+      echo "starting retrieveing all repo urls"
+
+      rm cfcrregistrfinder_step2.log
+      rm cfcrregistrfinder_stpe2.err
+      rm -r cfpips
+      mkdir cfpips
+
+      //get all the pipeline ids
+      pipsMetaUrl="https://g.codefresh.io/api/pipelines?limit=200&offset=0"
+      allPipsMetadata="cfpips/"$2"_allPipsMetadata"
+
+      echo '****** Step .1) Invoking '$pipsMetaUrl' to collect all pipelines detailed metadata into a file '$allPipsMetadata"All.json"
+      #rm cfpips/$allPipsMetadata".json"
+      functionApiCall $pipsMetaUrl $1 $allPipsMetadata"All.json" true
 
 
-  allPipsMetadata=$2"_allPipsMetadata"
+      echo '****** Step 1) generating '$allPipsMetadata"_metadata.json file containing only pipeline metadata available"
 
-   echo '****** Step .1) Invoking '$pipsMetaUrl' to collect all pipelines detailed metadata into a file '$allPipsMetadata".json"
-  rm $allPipsMetadata".json"
-  functionApiCall $pipsMetaUrl $1 $allPipsMetadata".json" true
-
-  status=`cat $allPipsMetadata".json" | jq '.status'`
-  echo $status
-  if [ $status -eq 401 ]
-   then
-      echo " Authenticaiton error.Please check your api key and try again"
-      cat $allPipsMetadata".json"
-      exit 1;
-   fi
-
-  echo '****** Step 1) generating '$allPipsMetadata"_repolocations.json file containing all inline contents available"
-  cat $allPipsMetadata".json" |jq '[.docs[]| {meta : {name: .metadata.name,project:.metadata.project,pipelineid:.metadata.id},stepsJson:.spec.steps,specTemplate:.spec.specTemplate}|select(.stepsJson != {})]' > $allPipsMetadata"_allinline.json"
+      cat $allPipsMetadata"All.json" |jq '[.docs[]| {meta : {name: .metadata.name,project:.metadata.project,pipelineid:.metadata.id}}]' > $allPipsMetadata"_metadata.json"
  
-  echo '****** Step 2) generating '$allPipsMetadata"_repolocations.json file containing all repo based pipeline yaml meatadats"
-  cat $allPipsMetadata".json" |jq '[.docs[]| {meta : {name: .metadata.name,project:.metadata.project,pipelineid:.metadata.id},stepsJson:.spec.steps,specTemplate:.spec.specTemplate}|select(.specTemplate != null and .stepsJson == {})]' > $allPipsMetadata"_repolocations.json"
- 
-#   echo '****** Step 3) generating '$allPipsMetadata"_inline.json file containing all inline  pipeline yaml meatadata"
-#   cat $allPipsMetadata".json" |jq '[.docs[]| {meta : {name: .metadata.name,project:.metadata.project,pipelineid:.metadata.id},stepsJson:.spec.steps,specTemplate:.spec.specTemplate}|select(.specTemplate == null)]' > $allPipsMetadata"_inline.json"
- 
- 
-  echo "repo metadat retrival completed"
-  a=0;
+     # get the last build per pipeline
+
+      echo '****** Step 2) generating '$allPipsMetadata"_metadata.json file containing only pipeline metadata available"
+
+      #cat $allPipsMetadata".json" |jq '[.docs[]| {meta : {name: .metadata.name,project:.metadata.project,pipelineid:.metadata.id}}]' > $allPipsMetadata"_metadat.json"
+      a=1
+      jq -c '.[]' $allPipsMetadata"_metadata.json" | while read row; do
+        pip_id=`echo  $row | jq -c '.meta.pipelineid'|sed 's/\"//g'`
+        #   echo 'excrted pipid'$pip_id
+        pip_name=`echo  $row | jq -c '.meta.name'|sed 's/\"//g'`
+        
+        if [ $a -eq 250 ]
+        then
+          echo "processing pipline no" $a  "pipelin id " $pip_id " name : " $pip_name " and breaking the loop"
+          break
+        #     
+        fi
+        a=`expr $a + 1`;
 
 
+        pipBuildUrl="https://g.codefresh.io/api/workflow/?limit=1&page=1&pageSize=1&pipeline="$pip_id
+        filaname="${pip_name////_}_$pip_id"
+        mfilename="cfpips/"$filaname"_meta.json"
+        functionApiCall $pipBuildUrl $1 $mfilename true
+        finalWorkflowYaml=$(cat $mfilename| jq '.workflows.docs[0].finalWorkflowYaml' | sed 's/\\n/\'$'\n''/g' | sed -e 's/^"//' -e 's/"$//')
+        #echo "$finalWorkflowYaml"   
+        yfilename="cfpips/"$filaname".yaml"
+        echo "$finalWorkflowYaml" > $yfilename
+        $(yq -j r $yfilename| jq '.| {spec:.steps}|.[]' > "cfpips/"$filaname".json")
+      done
+     echo "processed " $a  " no of pipelines from the metadata repsong into the folder cfpips"
+       
+
+}
 
 
-  
-  echo "delete a folder by name,cfpips.If you don't wan the script to delete it,please delete it manually "
-  rm -r cfpips
-  mkdir cfpips
+function2(){
 
 
-  rm cfcrregistrfinder.log
-  rm cfcrregistrfinder.err
+  a=1
+  rm cfcrregistrfinder_step2.log
+  rm cfcrregistrfinder_stpe2.err
   rm $2_pipidentification_Report.csv
-
-   headerrecord="Pipeline ID,Pipeline Name,Step Type ,Step Name , Step Definition"
-   echo $headerrecord >> $2_pipidentification_Report.csv
-
-   echo '**** processing inline yamls from repo meta *****'
-   a=1
-   jq -c '.[]' $allPipsMetadata"_allinline.json" | while read row; do
-
-      #echo $row| jq -c '.stepsJson'
-
-  #echo "next row no "$a
-  a=`expr $a + 1`
-
-if [ $a -eq 8 ]
-then
-   echo $row;
-fi
-#   echo "next row extraction starts"
-
-#   echo  $row | jq -c '.meta.pipelineid'
-  pip_id=`echo  $row | jq -c '.meta.pipelineid'|sed 's/\"//g'`
-#   echo 'excrted pipid'$pip_id
-  pip_name=`echo  $row | jq -c '.meta.name'|sed 's/\"//g'`
-#   echo 'excrted pip_name'$pip_name
-
-  build_type_ref=`echo $row|jq '.stepsJson|with_entries( select(.value.type == "build"))'`
-  push_cfcr_type_ref=`echo $row|jq '.stepsJson|with_entries( select(.value.registry == "cfcr"))'`
-
-#   echo 'excrted build_type_ref'
-
-      if [ "$build_type_ref" != "{}" ]; then
-        #echo ' build_type_ref looks good'
-                    step_name=`echo $build_type_ref |jq  '.|keys[0]'`
-                  
-                    JSON_STRING=`echo  $build_type_ref |sed 's/\,/\^/g'`;
-                   # echo $JSON_STRING 
-                    nextpipoutput=$pip_id","$pip_name",build,"$step_name","$JSON_STRING
-                    echo $nextpipoutput >> $2_pipidentification_Report.csv
-      else
-                    echo "no build step found"$build_type_ref >> cfcrregistrfinder.log
-      fi
-
-      if [ "$push_cfcr_type_ref" != "{}" ]; then
-                    step_name=`echo $push_cfcr_type_ref |jq  '.|keys[0]'`
-                    JSON_STRING=\'$push_cfcr_type_ref\'
-                    echo $JSON_STRING 
-                    nextpipoutput=$pip_id","$pip_name",push,"$step_name","$JSON_STRING
-
-
-                    echo $nextpipoutput >> $2_pipidentification_Report.csv
-      else
-                   echo $push_cfcr_type_ref >> cfcrregistrfinder.log
-      fi
-   done
-#    exit 1
-
-
-
-   echo "limiting no pip processing to "$3
-  jq -c '.[]' $allPipsMetadata"_repolocations.json" | while read row; do
-
-   echo "Processing pipeline no " $a 
-   if [ $a -eq $3 ]
-   then
-      break
-   fi
-   a=`expr $a + 1`
-
-
-   repo_loc=`echo  $row | jq -c '.specTemplate.repo'|sed 's/\"//g'`;
-   echo 'repo_loc -> '$repo_loc  >> cfcrregistrfinder.log
-#             #repo_revision=`echo  $row | jq -c '.specTemplate.revision'`;
-
-    repo_revision=`echo  $row | jq -c '.specTemplate.revision'|sed 's/\"//g'`;
-    echo 'repo_revision -> '$repo_revision >> cfcrregistrfinder.log
+  allPipsMetadata=$2"_allPipsMetadata"
+  a=1
+  cat cfpips/$allPipsMetadata"_metadata.json"|jq -c '.[]'| while read row; do
     
+    pip_id=`echo  $row | jq -c '.meta.pipelineid'|sed 's/\"//g'`
+        #   echo 'excrted pipid'$pip_id
+    pip_name=`echo  $row | jq -c '.meta.name'|sed 's/\"//g'`
+    if [ $a -eq 250 ]
+        then
+          #echo "processing pipline no" $row  "pipelin id " $pip_id " name : " $pip_name
+          break
+    else 
+         echo "processing pipline no "$a" pipelin id: " $pip_id ", name : " $pip_name
+         
+    fi
+     a=`expr $a + 1`;
+    filaname="${pip_name////_}_$pip_id"
 
-     if [ ${repo_revision}  = "null"  ]; then
-
-                repo_revision="master";
-
-     fi
-     repo_revision=`echo  $repo_revision | sed 's/\//%2F/g'`;
-     echo 'repo_revision -> '$repo_revision >> cfcrregistrfinder.log
-
-     repo_path=`echo  $row | jq -c '.specTemplate.path'|sed 's/\.//'|sed 's/\///'|sed 's/\"//g'|sed 's/\//%2F/g'`;
-     echo 'repo_path' $repo_path >> cfcrregistrfinder.log
-
-     repo_context=`echo  $row | jq -c '.specTemplate.context'|sed 's/\"//g'`;
-
-     repo_url='https://g.codefresh.io/api/repos/'$repo_loc'/'$repo_revision'/'$repo_path'?context='$repo_context
-     echo "repo-url --> "$repo_url >> cfcrregistrfinder.log
-     pip_id=`echo  $row | jq -c '.meta.pipelineid'|sed 's/\"//g'`
-     pip_name=`echo  $row | jq -c '.meta.name'|sed 's/\"//g'`
-
-     echo 'pid id '$pip_id 
-     echo 'pid id '$pip_id  >> cfcrregistrfinder.log
-
-     filaname="${repo_loc////_}_$pip_id"
-
-
-     functionApiCall $repo_url $1 "cfpips/"$filaname".yaml"  false
-
-
-    #  curl -X GET $repo_url \
-    #                 -H 'x-access-token: '$accessToken  > "cfpips/"$filaname".yaml" 
-                   # $repo_url  > "cfpips/"$filaname".yaml"
-      sleep 1
-      rm fil1.yaml
-      cp "cfpips/"$filaname".yaml" fil1.yaml
-      repo_yaml=`yq -j r fil1.yaml| jq '.| {spec:.steps}|.[]' > "cfpips/"$filaname".json"`
-      f="cfpips/"$filaname".json"
-      if [[ -s $f ]]; then 
+    f="cfpips/"$filaname".json"
+    if [[ -s $f ]]; then 
         #  echo "file has something"; 
             firstline=`tail -1 $f`
-            echo "firstline" $firstline  >> cfcrregistrfinder.log
+            echo "firstline" $firstline  >> cfcrregistrfinder_step2.log
             if [ "$firstline" != "null" ]; then
                
                 #echo 'next json file '$f ',cfcr references --> '`cat $f |jq '.|with_entries( select(.value.type == "build" or .value.registry == "cfcr" ))'`;
@@ -197,7 +123,7 @@ fi
                     nextpipoutput=$pip_id","$pip_name",build,"$step_name","$JSON_STRING
                     echo $nextpipoutput >> $2_pipidentification_Report.csv
                 else
-                    echo $build_type_ref
+                    echo 'no type:build in pipeline '$pip_name >> cfcrregistrfinder_step2.log
                 fi
 
                  
@@ -210,51 +136,72 @@ fi
 
                     echo $nextpipoutput >> $2_pipidentification_Report.csv
                 else
-                   echo $push_cfcr_type_ref
+                   echo 'no push:type registry:cfcr in pipeline '$pip_name >> cfcrregistrfinder_step2.log
                 fi
-               
+
+
+                cfcrrep=$(cat $f|grep -o '"r.cfcr.io/[^ ]*\"'|sed 's/\"//g'|head -1)
+
+                if [ ! -z "$cfcrrep" ]; then 
+                    echo $pip_name' contains... reference to a r.cfcr.io image '$cfcrrep  #>> cfcrregistrfinder_step2.log
+
+                    
+                    pull_cfcr_type_ref=`cat $f |jq --arg cfcrref $cfcrrep '.|with_entries( select(.value.image == $cfcrref))'`
+
+                   # echo $pull_cfcr_type_ref
+                
+                    if [ "$pull_cfcr_type_ref" != "{}" ]; then
+                        step_name=`echo $pull_cfcr_type_ref |jq  '.|keys[0]'`
+                        JSON_STRING=\'$pull_cfcr_type_ref\'
+                        #echo $JSON_STRING 
+                        nextpipoutput=$pip_id","$pip_name",pull,"$step_name","$JSON_STRING
+                        echo $nextpipoutput >> $2_pipidentification_Report.csv
+                    else
+                    #echo 'no push:type registry:cfcr in pipeline '$pip_name >> cfcrregistrfinder_step2.log
+                        
+                        pull_cfcr_pip=`cat $f |jq '.'`
+                        nextpipoutput=$pip_id","$pip_name",pull,step_error,"$pull_cfcr_pip
+                        echo $nextpipoutput >> $2_pipidentification_Report.csv
+                    fi
+                else
+                   echo $pip_name' does not contains reference to a r.cfcr.io image '$cfcrrep  >> cfcrregistrfinder_step2.log
+
+                fi
 
 
             else
              
-                echo "invalid json in $f"  >> cfcrregistrfinder.err
-                echo "repo-url --> "$repo_url >> cfcrregistrfinder.err
-                head -5 fil1.yaml >>  cfcrregistrfinder.err
-                echo "" >>  cfcrregistrfinder.err
+                echo "invalid json in $f"  >> cfcrregistrfinder_stpe2.err
+               
+               # head -5 fil1.yaml >>  cfcrregistrfinder_stpe2.err
+                echo "" >>  cfcrregistrfinder_stpe2.err
 
             fi
       else 
-            echo "file : $f is empty";  >> cfcrregistrfinder.err
-            echo "repo-url --> "$repo_url >> cfcrregistrfinder.err
-            head -5 fil1.yaml >>  cfcrregistrfinder.err
-            echo "" >>  cfcrregistrfinder.err
+            echo "file : $f is empty";  >> cfcrregistrfinder_stpe2.err
+           # head -5 fil1.yaml >>  cfcrregistrfinder_stpe2.err
+            echo "" >>  cfcrregistrfinder_stpe2.err
 
-      fi
-
-
-
-
-
+    fi
 
 
   done
 
-  rm fil1.yaml
-
-
 }
+# process all json files for the build, push , pull patterns
 
-
-
-
-#function1 $1 $2 $3
 
 if [ $# -eq 3 ]; then
    # echo "Your command line contains $# arguments"
-   function1 $1 $2 $3
-else
-    echo "usage ./cfextractor.sh API_KEY project_name pipeline_limit_count"
+  
+    # prepare data content. Metadata, yaml and json files for a pipeline
+    function1 $1 $2 $3
+    # run regex against the pipeline to identify the changes
     
+    function2 $1 $2 $3
+  #  function3 $1 $2 $3
+else    
+   echo "please run the script passing our codefresh api key as ./cfbuld.sh <api_key> account_shorthandname pipelin limit"
 fi
 
 
